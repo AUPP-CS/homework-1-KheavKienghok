90f6cdede3d7b5c1792cf5543b092eafc57b62b2
w3schools-test <2023500Kheav@aupp.edu.kh>
2024-01-26 14:50:54 +0700

    Homework

--- currency_conversion.py
+++ currency_conversion.py
@@ -1,5 +1,17 @@
 def currency_conversion(currency, amount):
     # Add your code here
+
+    if amount.replace('.', '', 1).isdigit():
+        if currency == "usd":
+            return float(amount) * 4075
+        elif currency == "yuan":
+            return float(amount) * 575
+        elif currency == "baht":
+            return float(amount) * 115
+        else:
+            return "not found"
+    else:
+        return "invalid amount"
     
     # Reminders: 
     # - currency is a string that tells us what currency to change to. It can be 'usd', 'yuan', or 'baht'.
--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from currency_conversion import currency_conversion
 
 # Print a welcome message and list of valid currencies
+print("╔════════════════════════════════════════════════════════════════════════════╗")
+print("║              Welcome to the AUPP Currency Conversion to RIEL APP           ║")
+print("║----------------------------------------------------------------------------║")
+print("║                                                                            ║")
+print("║                            Instructions:                                   ║")
+print("║                                                                            ║")
+print("║    1. Enter the currency you want to convert from. (USD $, YUAN ¥, BAHT ฿) ║")
+print("║    2. Enter the ammount you want to convert.                               ║")
+print("║    3. The ammount will be converted to RIEL (៛).                           ║")
+print("║                                                                            ║")
+print("╚════════════════════════════════════════════════════════════════════════════╝")
+print("\n")
 
-# Ask the user for the currency they want to convert from
+# Ask the user for the currency they want to conusvert from
+currency_store = input("Enter the currency: ").lower()
 
 # Ask the user for the amount they want to convert
+currency_amount = input("Enter the amount  : ")
 
 # Call the currency_conversion function with the user's currency and amount
+result = currency_conversion(currency_store, currency_amount)
+
+riel_cambodia = "\u17DB"
+print("\n╔════════════════════════════════════════════════════════════════════════════╗")
+print("║                                                                            ║")
+print(f"║           Your total in RIEL៛ after conversion is:   {result:<15}       ║")
+print("║                                                                            ║")
+print("╚════════════════════════════════════════════════════════════════════════════╝")
